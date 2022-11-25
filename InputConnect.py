f9119e5d8ee15ca8b2cb2bc90a47c33f1f78929e
Harizon <dp200308@gmail.com>
2022-11-25 19:24:30 +0500

    add table print

--- InputConnect.py
+++ InputConnect.py
@@ -3,25 +3,87 @@ import os
 from DataSet import DataSet
 from Report import Report
 from Statistics import Statistic
+from Table import Table
 
+russian = {
+    "name": "Название",
+    "description": "Описание",
+    "key_skills": "Навыки",
+    "experience_id": "Опыт работы",
+    "premium": "Премиум-вакансия",
+    "employer_name": "Компания",
+    "salary_from": "Нижняя граница вилки оклада",
+    "salary_to": "Верхняя граница вилки оклада",
+    "salary_gross": "Оклад указан до вычета налогов",
+    "salary_currency": "Идентификатор валюты оклада",
+    "salary": "Оклад",
+    "salary_average": "Средняя зарплата",
+    "area_name": "Название региона",
+    "published_at": "Дата публикации вакансии",
+    "noExperience": "Нет опыта",
+    "between1And3": "От 1 года до 3 лет",
+    "between3And6": "От 3 до 6 лет",
+    "moreThan6": "Более 6 лет",
+    "AZN": "Манаты",
+    "BYR": "Белорусские рубли",
+    "EUR": "Евро",
+    "GEL": "Грузинский лари",
+    "KGS": "Киргизский сом",
+    "KZT": "Тенге",
+    "RUR": "Рубли",
+    "UAH": "Гривны",
+    "USD": "Доллары",
+    "UZS": "Узбекский сум",
+    "True": "Да",
+    "False": "Нет",
+}
 
 class InputConnect:
     def __init__(self):
-        self.file_name = input('Введите название файла: ').strip()
-        self.work_name = input('Введите название профессии: ').strip()
-        if self.is_valid():
-            dataset: DataSet = DataSet(self.file_name)
-            stats = Statistic(dataset, self.work_name)
-            stats.print_statistic()
-            report = Report(stats)
-            report.generate_excel()
-            report.generate_image()
-            report.generate_pdf()
+        selection = input('Вакансии или Статистика: ').strip()
+        if selection == "Вакансии":
+            self.file_name = input('Введите название файла: ').strip()
+            self.filter_data = list(filter(lambda e: e != "", input('Введите параметр фильтрации: ').split(': ')))
+            self.sort_key = input('Введите параметр сортировки: ').strip()
+            self.is_reversed_sort = input('Обратный порядок сортировки (Да / Нет): ').strip()
+            self.table_ranges = list(map(int, input('Введите диапазон вывода: ').split()))
+            self.fields = list(filter(lambda e: e != "", input('Введите требуемые столбцы: ').split(', ')))
+            self.__translation: dict[str, str] = russian
+            if self.is_valid(True):
+                self.table = Table(self.file_name, self.filter_data, self.sort_key, self.is_reversed_sort,
+                                   self.table_ranges, self.fields, russian)
+                self.table.filter_vacancies()
+                self.table.sort_vacancies()
+                self.table.print_table()
+        elif selection == "Статистика":
+            self.file_name = input('Введите название файла: ').strip()
+            self.work_name = input('Введите название профессии: ').strip()
+            if self.is_valid():
+                dataset: DataSet = DataSet(self.file_name)
+                stats = Statistic(dataset, self.work_name)
+                stats.print_statistic()
+                report = Report(stats)
+                report.generate_excel()
+                report.generate_image()
+                report.generate_pdf()
 
-    def is_valid(self) -> bool:
+    def is_valid(self, isTable=False) -> bool:
         if os.stat(self.file_name).st_size == 0:
             print("Пустой файл")
             return False
+        if isTable:
+            if len(self.filter_data) == 1:
+                print('Формат ввода некорректен')
+                return False
+            elif len(self.filter_data) and not self.filter_data[0] in self.__translation.values():
+                print('Параметр поиска некорректен')
+                return False
+            elif self.sort_key and self.sort_key not in self.__translation.values():
+                print('Параметр сортировки некорректен')
+                return False
+            elif self.is_reversed_sort and self.is_reversed_sort != 'Да' and self.is_reversed_sort != 'Нет':
+                print('Порядок сортировки задан некорректно')
+                return False
         return True
 
     
---
+++ Table.py
@@ -0,0 +1,111 @@
+import re
+from datetime import datetime
+from prettytable import PrettyTable, ALL
+from DataSet import DataSet
+from Vacancy import Vacancy
+
+
+class Table:
+    def __init__(self, file_name, filter_data, sort_key, is_reversed_sort, table_ranges, fields, translation):
+        self.file_name = file_name
+        self.filter_data = filter_data
+        self.sort_key = sort_key
+        self.is_reversed_sort = is_reversed_sort
+        self.table_ranges = table_ranges
+        self.fields = fields
+        self.__translation: dict[str, str] = translation
+        self.__reversed_translation = dict([(v, k) for k, v in self.__translation.items()])
+        self.dataset = DataSet(self.file_name)
+
+    def filter_vacancies(self):
+        if not len(self.filter_data):
+            return
+        filtered: list[Vacancy] = []
+        for vac in self.dataset.vacancies_objects:
+            if self.__reversed_translation[self.filter_data[0]].startswith('salary_'):
+                vacancy_filter_value = getattr(vac.salary,
+                                               self.__reversed_translation[self.filter_data[0]])
+            else:
+                vacancy_filter_value = getattr(vac, self.__reversed_translation[self.filter_data[0]])
+            if self.filter_data[0] == self.__translation['key_skills']:
+                if not all([k in vacancy_filter_value for k in self.filter_data[1].split(', ')]):
+                    continue
+            elif self.filter_data[0] == self.__translation['salary']:
+                int_value = int(float(self.filter_data[1]))
+                if vac.salary.salary_from > int_value or int_value > vac.salary.salary_to:
+                    continue
+            elif self.filter_data[0] == self.__translation['published_at']:
+                if ".".join(vac.published_at[:10].split('-')[::-1]) != self.filter_data[1]:
+                    continue
+            elif self.filter_data[1] in self.__reversed_translation:
+                if vacancy_filter_value != self.__reversed_translation[self.filter_data[1]]:
+                    continue
+            elif vacancy_filter_value != self.filter_data[1]:
+                continue
+            filtered.append(vac)
+        if not filtered:
+            print('Ничего не найдено')
+        self.dataset.vacancies_objects = filtered
+
+    def sort_vacancies(self):
+        if not self.sort_key and self.dataset.vacancies_objects:
+            return
+        is_reversed = True if self.is_reversed_sort == 'Да' else False
+        if self.sort_key == self.__translation['key_skills']:
+            def sort_func(vac: Vacancy):
+                return len(vac.key_skills)
+        elif self.sort_key == self.__translation['experience_id']:
+            def sort_func(vac: Vacancy):
+                return re.sub(r'\D', '', vac.experience_id)
+        elif self.sort_key == self.__translation['salary']:
+            def sort_func(vac: Vacancy):
+                return vac.salary.convert_currency_average()
+        elif self.sort_key == self.__translation['published_at']:
+            def sort_func(vac):
+                return datetime.strptime(getattr(vac, 'published_at'),
+                                         "%Y-%m-%dT%H:%M:%S%z")
+        else:
+            def sort_func(vac):
+                return getattr(vac, self.__reversed_translation[self.sort_key])
+        self.dataset.vacancies_objects.sort(key=sort_func, reverse=is_reversed)
+
+    def print_table(self):
+        if not self.dataset.vacancies_objects:
+            return
+        table = PrettyTable()
+        table.hrules = ALL
+        table.align = 'l'
+        table.field_names = ['№', *[self.__translation[k] for k in self.dataset.vacancies_objects[0].__dict__.keys()]]
+        table._max_width = {i: 20 for i in table.field_names}
+        c = 1
+        nl = '\n'
+
+        for vacancy in self.dataset.vacancies_objects:
+            row = [c]
+            for header, value in vacancy.__dict__.items():
+                if header == 'key_skills':
+                    row_value = nl.join(value)
+                    row.append(f"{row_value[:100]}{'...' if len(row_value) > 100 else ''}")
+                elif header == 'published_at':
+                    row.append(".".join(value[:10].split('-')[::-1]))
+                elif header == 'salary':
+                    row.append(
+                        f'{"{:,}".format(value.salary_from).replace(",", " ")} - '
+                        f'{"{:,}".format(value.salary_to).replace(",", " ")} '
+                        f'({self.__translation[value.salary_currency]}) '
+                        f'({"Без вычета налогов" if value.salary_gross == "True" else "С вычетом налогов"})')
+                else:
+                    row_value = value
+                    row.append(self.__translation[value] if value in self.__translation
+                               else f"{row_value[:100]}{'...' if len(row_value) > 100 else ''}")
+            table.add_row(row)
+            c += 1
+
+        if len(self.table_ranges) == 0:
+            self.table_ranges.append(1)
+            self.table_ranges.append(table.rowcount + 1)
+        elif len(self.table_ranges) == 1:
+            self.table_ranges.append(table.rowcount + 1)
+
+        print(table.get_string(start=self.table_ranges[0] - 1, end=self.table_ranges[1] - 1,
+                               fields=['№', *self.fields] if len(self.fields) > 0 else table.field_names))
